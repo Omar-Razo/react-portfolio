31681696391c0fd9059776b65de25cc54b9bcf91
Omar Razo <osrazo@ucdavis.edu>
2023-11-14 19:22:48 -0800

    basic footer created

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -2,12 +2,14 @@
 
 export default function Footer () {
     return (
-        <div>
-            <ul>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-            </ul>
-        </div>
+        <footer className="">
+            <div className="">
+                <ul>
+                    <li><a href="http://" target="_blank" ><img src="https://commons.wikimedia.org/wiki/File:Cib-linkedin_(CoreUI_Icons_v1.0.0).svg" alt="LinkedIn logo" /></a></li>
+                    <li><a href="https://github.com/Omar-Razo" target="_blank" ><img src="https://commons.wikimedia.org/wiki/File:GitHub_Invertocat_Logo.svg" alt="GitHub logo" /></a></li>
+                    <li><a href="http://" target="_blank" ><img src="https://commons.wikimedia.org/wiki/File:X_logo_2023.svg" alt="twitter x logo" /></a></li>
+                </ul>
+            </div>
+        </footer>
     )
 }
--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function NavTabs() {
         <ul className='nav nav-tabs'>
         <li className='nav-item'>
             <Link
-            to='/About'
+            to='/About '
             // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === "/About" ? "nav-link active" : "nav-link"}
             >
