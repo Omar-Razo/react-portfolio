4570a74773ed5ca77175bf3e7e2ef6c729b956bb
Omar Razo <girostrike@gmail.com>
2023-11-19 12:03:33 -0800

    added css to main. footer classes updated(wrong branch i know)

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import App from './App';
@@ -10,8 +11,6 @@ import AboutMePage from './pages/AboutMePage';
 import PortfolioPage from './pages/PortfolioPage';
 import ContactPage from './pages/ContactPage';
 import ResumePage from './pages/ResumePage';
-
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const router = createBrowserRouter([
   {
