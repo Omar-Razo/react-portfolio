bab8d7a52f030a150862c430b1db40ad8ee6f17a
Omar Razo <osrazo@ucdavis.edu>
2023-11-21 17:22:37 -0800

    fixed img path for netlify deploy

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -7,7 +7,7 @@ const AboutMePage = () => {
     return (
         <div className="" >
         <h1 className="">About Me</h1>
-        <img src="src\assets\98504336.jfif" alt="unamused cat with a Bluetooth device on its head" style={imgStyle}/>
+        <img src="./98504336.jfif" alt="unamused cat with a Bluetooth device on its head" style={imgStyle}/>
         <p className="my-5">I am currently working my way through the UC Davis Full-Stack Bootcamp. I've dabbled in web development prior, but now I'm ready to make a career out of my interests. I draw from all experiences and aspects of my life and apply them to problem solving and app development.</p>
         <p className="">As former UC Davis student, I believe asking questions is the path to understanding. I need to break it so I can learn how to fix it.</p>
         </div>
